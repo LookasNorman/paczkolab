f95d6be8dcf01a533fc8e69ba7c686b47f49e240
LookasNorman <lookasziebice@gmail.com>
2019-06-17 16:41:49 +0200

    Router - load class

--- router.php
+++ router.php
@@ -1,5 +1,18 @@
 <?php
 
-//Load all class
+//Load DB class
+use DBmysql;
+//parsing URL
+$uriPathInfo = $_SERVER['REQUEST_URI'];
+//explode path info
+$path = explode('/', $uriPathInfo);
+$requestClass = $path[1];
+//class name
+$requestClass = preg_replace('#[^0-9a-zA-Z]#', '', $requestClass);//remove all non alfanum chars from request
+$className = ucfirst(strtolower($requestClass));
+//Load class
+use $className;
+
+//Load DB config
 require (__DIR__ . '/config.php');
 
