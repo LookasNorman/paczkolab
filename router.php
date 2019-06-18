29f0d3b91381535d65d942d4235a68d2e5ed54e8
LookasNorman <lookasziebice@gmail.com>
2019-06-18 16:15:20 +0200

    Router - delete bug from file

--- router.php
+++ router.php
@@ -10,8 +10,6 @@ $requestClass = $path[1];
 //class name
 $requestClass = preg_replace('#[^0-9a-zA-Z]#', '', $requestClass);//remove all non alfanum chars from request
 $className = ucfirst(strtolower($requestClass));
-//Load class
-use $className;
 
 //Load DB config
 require (__DIR__ . '/config.php');
