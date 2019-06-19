31e3e556dc036280c60f4990267a5349baec1e60
LookasNorman <lookasziebice@gmail.com>
2019-06-20 00:04:09 +0200

    Require class/className
    if no DB error - require RestEndPoints/className

--- router.php
+++ router.php
@@ -21,13 +21,15 @@ $requestClass = $path[2];
 $requestClass = preg_replace('#[^0-9a-zA-Z]#', '', $requestClass);//remove all non alfanum chars from request
 $className = ucfirst(strtolower($requestClass));
 
-$classFile = __DIR__.'/restEndPoints/'.$className.'.php';
+$classFile = __DIR__.'/class/'.$className.'.php';
 require_once $classFile;
 
 ######### END DYNAMIC LOAD #########
 
 $pathId = isset($path[3]) ? $path[3] : null;
-
+if (!isset($response['error'])) {//process request if no db error
+    include_once __DIR__.'/restEndPoints/'.$className.'.php';
+}
 
 header('Content-Type: application/json');//return json header
 
